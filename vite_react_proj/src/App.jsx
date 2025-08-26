709720e4e2583c995f2c8bfd7c9ca40c049f42dc
butny76 <buntyavchhare@gmail.com>
2025-08-26 23:15:40 +0530

    fist hook & mult places ui or state update same time

--- vite_react_proj/src/App.jsx
+++ vite_react_proj/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let [count,setcount]=useState(0)
+
+  const addOne=()=>{
+    count=count+1
+    setcount(count)
+    console.log(count)
+  }
   return (
     <>
       <h1>hello world!!</h1>
+      <div className="counter-div"> counter:{count}</div>
+      <button onClick={addOne}>add one {count}</button>
     </>
-  )
+  );
 }
 
 export default App
